7b6a1e7e4be52817d27c5524e964580ceb8057a8
Attila Fekete <feketeattila89@gmail.com>
2019-06-17 15:08:48 +0200

    d1

---
+++ src/Future/Future.java
@@ -0,0 +1,14 @@
+package Future;
+
+import java.sql.SQLOutput;
+
+public class Future {
+
+  public static void main(String[] args) {
+
+  }
+
+  public void future () {
+    System.out.println("Rák");
+  }
+}
